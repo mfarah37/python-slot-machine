6cd34c1ac87b7f4cf85b426102c3454c58de3efa
Mohamed <mfarah37@gmail.com>
2022-09-25 08:20:13 -0700

    adds user input functionality & prints slot machine with filled slots

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 1000
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbol):
+    all_symbols = []
+    for symbol, symbol_count in symbol.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    
+    columns = []
+    for _ in range(cols):
+        column = []
+        # Copy of list(NOT REFERENCE) [:]
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        
+        columns.append(column)
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        # Empty print() creates space so the next column prints on new line
+        print()
 
 def deposit():
     while True:
@@ -71,5 +112,8 @@ def main():
     print(
         f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}"
         )
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+
 main()
 
